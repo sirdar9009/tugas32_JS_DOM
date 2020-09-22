5adc8ba7036e892f6ce08138be600351be599183
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-09-22 09:19:24 +0700

    Add files via upload

---
+++ get_element.js
@@ -0,0 +1,9 @@
+function panggilEksternal(){
+		var namadep = document.getElementById('namadep')
+		var namabel = document.getElementById('namabel')
+		var hobi = document.getElementById('hobi')
+
+		console.log(namadep)
+		console.log(namabel)
+		console.log(hobi)
+}
